286766404b9c81429b8f3071766ea19b59d7cb2b
panic <panic@d463407e-c81c-0410-9dde-f82ba9d86a8d>
2007-02-13 10:48:23 +0000

    Added automatic restart pipe creation, and copyright notice.
    git-svn-id: svn+ssh://ssh.itu.dk/import/stud/share/projects/bpl/svn/trunk/impl@488 d463407e-c81c-0410-9dde-f82ba9d86a8d

---
+++ bpl/src/apps/bplweb/webserver/start.sh
@@ -0,0 +1,43 @@
+#! /bin/sh
+
+# Copyright (c) 2007  The BPL Group at the IT University of Copenhagen
+
+# This file is part of BPL.
+
+# BPL is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+
+# BPL is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with BPL; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+# USA
+
+
+# Start the web server and listen to the restart pipe:
+# whenever a line of arbitrary text is read from the pipe,
+# restart the server.
+
+if [ -e restart ]
+  if [ ! -p restart ] 
+  then
+    echo "Error: file `restart' should be a named pipe!"
+    exit 1
+  fi
+else
+  mkfifo -m ug=rw restart
+fi
+
+while true
+do
+  ruby script/server -p 7913 >> log/webserver.log 2>&1 &
+  pid=$!
+  read < restart
+  kill $pid
+done
