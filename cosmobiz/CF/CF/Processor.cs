7f6a0de765083d1bf333d00bc631b42ac4989440
luzker <luzker@d463407e-c81c-0410-9dde-f82ba9d86a8d>
2007-06-28 13:21:49 +0000

    git-svn-id: svn+ssh://ssh.itu.dk/import/stud/share/projects/bpl/svn/trunk/impl@821 d463407e-c81c-0410-9dde-f82ba9d86a8d

--- cosmobiz/CF/CF/Processor.cs
+++ cosmobiz/CF/CF/Processor.cs
@@ -6,5 +6,33 @@ namespace CF
 {
     class Processor
     {
+        ListedElements elements;
+        public Processor(ListedElements elements)
+        {
+            this.elements = elements;
+        }
+
+        public void ProcessElements()
+        {
+            foreach (Element elem in elements)
+            {
+                switch (elem.Type)
+                {
+                    case "sequence":
+                        { }
+                    case "endsequence":
+                        { }
+                    case "activity":
+                        { }
+                    case "Flow":
+                        { }
+                    case "endFlow":
+                        { }
+
+                    default:
+                        break;
+                }
+            }
+        }
     }
 }
