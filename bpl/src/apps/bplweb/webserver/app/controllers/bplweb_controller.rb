6c8d9b8f940765eba71a71c1d4d95137af10b905
panic <panic@d463407e-c81c-0410-9dde-f82ba9d86a8d>
2007-11-30 14:19:31 +0000

    New BPLweb Demo Example
    git-svn-id: svn+ssh://ssh.itu.dk/import/stud/share/projects/bpl/svn/trunk/impl@1326 d463407e-c81c-0410-9dde-f82ba9d86a8d

--- bpl/src/apps/bplweb/webserver/app/controllers/bplweb_controller.rb
+++ bpl/src/apps/bplweb/webserver/app/controllers/bplweb_controller.rb
@@ -16,9 +16,9 @@ class BplwebController < ApplicationController
                  "passive0(L), passive(L1 -: 1), passive(L2 -: 2), passive(L20 =: 2 --> 0)\n" +
                  "atomic0 (M), atomic (M1 -: 1), atomic (M2 -: 2)"
     @agent = "-//[e1,e2] o (\n" +
-             " K1[e1] o M `|` L1[e1] o M2[e1,e2] `|`
-             " K21[f][[b1,b2],[]] o (<[b1,b2]> L2[b1,b2] o M1[e2]) *\n"
-             " L20[][[b1],[b2]] o ([b1,b2]> L2[b1,b2])\n"
+             " K1[e1] o M `|` L1[e1] o M2[e1,e2] `|`" +
+             " K21[f][[b1,b2],[]] o (<[b1,b2]> L2[b1,b2] o M1[e2]) *\n" +
+             " L20[][[b1],[b2]] o ([b1,b2]> L2[b1,b2])\n" +
              ")"
     @simplifymatches = params[:simplifymatches] == "on"
     @rules = [Rule.new(:redex => 'K1[y1] || L1[y1] || (<[y2,y3]> L2[y2,y3])',
