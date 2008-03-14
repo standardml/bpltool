39c62fb6f318e089cbbae7fa4008397a283d4492
panic <panic@d463407e-c81c-0410-9dde-f82ba9d86a8d>
2008-03-14 10:06:43 +0000

    When code has seen evolution now make has a nice target potion for doing the job: upload to the web an updated, fresh distribution
    git-svn-id: svn+ssh://ssh.itu.dk/import/stud/share/projects/bpl/svn/trunk/impl@1790 d463407e-c81c-0410-9dde-f82ba9d86a8d

--- bpl/src/mktar.sh
+++ bpl/src/mktar.sh
@@ -2,22 +2,21 @@
 
 svn=svn
 
+programname=`basename "$0"`
 usage () {
-  echo Usage: $0 NAME VERSION
+  echo Usage: $programname NAME VERSION
   echo "  Create in the current directory a gzipped TAR file"
   echo "  NAME-VERSION.REVISION.tar.gz of the source files."
   echo "  Source files under SVN control are included, unless they"
   echo "  are listed in .tarexclude, which is local for each subdirectory."
   echo "  REVISION is the newest revision in the SVN repository."
+  exit $1
 }
 
-if [ -z "$2" ]
-then
-  usage; exit -1
-fi
+[ -z "$2" ] && usage 1
 
 case "$1" in
-  -h | -help | --help) usage; exit 0 ;;
+  -h | -help | --help) usage 0 ;;
 esac
 
 name="$1"
@@ -29,7 +28,7 @@ tar="$tarbase.tar.gz"
 if [ -e $tar ]
 then
   echo Error: $tar exists
-  exit -1
+  exit 1
 fi
 
 echo Building directory tree for archive $tar...
@@ -49,18 +48,19 @@ mksoftlinks () {
   dirs=`$svn list | grep / | $greptarexclude`
   for file in $files
   do
-  	ln -s "$up../$down$file" "$up$tarbase/$down$file" || exit -1
+  	ln -s "$up../$down$file" "$up$tarbase/$down$file" || exit 1
   done
   for dir in $dirs
   do
-    mkdir "$up$tarbase/$down$dir" || exit -1
-  	cd "$dir" || exit -1
+    mkdir "$up$tarbase/$down$dir" || exit 1
+  	cd "$dir" || exit 1
   	mksoftlinks "$down$dir" $up../
-  	cd .. || exit -1
+  	cd .. || exit 1
   done
 }
 
-mkdir $tarbase || exit -1
+mkdir $tarbase || exit 1
 mksoftlinks "" ""
 echo Creating archive $tar...
-tar czhf $tar $tarbase
+tar czhf $tar $tarbase || exit 1
+rm -r $tarbase || exit 1
