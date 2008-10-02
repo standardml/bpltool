3bc2c8b3bd560c8a9dbd7e4f02ca90e2c288ddcf
tiha <tiha@d463407e-c81c-0410-9dde-f82ba9d86a8d>
2008-10-02 10:54:12 +0000

    Removed load method (used by some test, but now included in the AbstractContextTest)
    git-svn-id: svn+ssh://ssh.itu.dk/import/stud/share/projects/bpl/svn/trunk/impl@2428 d463407e-c81c-0410-9dde-f82ba9d86a8d

--- beepell-dom/src/com/beepell/execution/bpel/Utils.java
+++ beepell-dom/src/com/beepell/execution/bpel/Utils.java
@@ -1,19 +1,12 @@
 package com.beepell.execution.bpel;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.namespace.QName;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
-import org.xml.sax.SAXException;
 
 import com.beepell.BPELConstants;
 
@@ -25,39 +18,7 @@ public class Utils {
 
     private static QName instanceQName = new QName(BPELConstants.BPI, "instance");
 
-    /**
-     * Load instance tree from file.
-     * 
-     * @param file The file to load.
-     * @return The instance Element node.
-     * @throws IOException If the file could not be loaded.
-     */
-    public static Element load(File file) throws IOException {
-        try {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            factory.setValidating(false);
-            factory.setCoalescing(false);
-            factory.setIgnoringComments(true);
-            factory.setIgnoringElementContentWhitespace(true);
-            factory.setNamespaceAware(true);
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document document;
 
-            document = builder.parse(file);
-            document.normalizeDocument();
-            Element element = document.getDocumentElement();
-            if (!isInstanceElement(element))
-                throw new IOException("File does not contain a process instance tree.");
-
-            return element;
-
-        } catch (SAXException exception) {
-            throw new IOException(exception.getLocalizedMessage(), exception);
-        } catch (ParserConfigurationException exception) {
-            throw new IOException(exception.getLocalizedMessage(), exception);
-        }
-
-    }
 
     /**
      * Returns true if the Element is an PBL instance Element.
