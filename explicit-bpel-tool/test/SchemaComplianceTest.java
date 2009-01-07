f0d40c2890fb3fa30964d9425afbadb92120ef42
tiha <tiha@d463407e-c81c-0410-9dde-f82ba9d86a8d>
2009-01-07 22:31:29 +0000

    Added XSLT schema and JUnit test to check Schema compliance.
    git-svn-id: svn+ssh://ssh.itu.dk/import/stud/share/projects/bpl/svn/trunk/impl@2771 d463407e-c81c-0410-9dde-f82ba9d86a8d

---
+++ explicit-bpel-tool/test/SchemaComplianceTest.java
@@ -0,0 +1,104 @@
+import java.io.File;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+import org.w3c.dom.Document;
+
+import junit.framework.TestCase;
+
+
+public class SchemaComplianceTest extends TestCase {
+
+	private Validator validator;
+	private DocumentBuilder builder;
+	
+    protected void setUp() throws Exception {
+		File file = new File("schemas/xslt20.xsd");
+		SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		Schema schema = factory.newSchema(file);
+		this.validator = schema.newValidator();
+		
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+		this.builder = dbf.newDocumentBuilder();
+		
+    }
+	
+	public final void testAttributes() throws Exception {
+		Document document = builder.parse(new File("xslt/attributes.xsl"));
+		this.validator.validate(new DOMSource(document));
+	}
+
+	public final void testDocumentation() throws Exception {
+		Document document = builder.parse(new File("xslt/documentation.xsl"));
+		this.validator.validate(new DOMSource(document));
+	}
+
+	public final void testExtensions() throws Exception {
+		Document document = builder.parse(new File("xslt/extensions.xsl"));
+		this.validator.validate(new DOMSource(document));
+	}
+	
+	public final void testGlobalScope() throws Exception {
+		Document document = builder.parse(new File("xslt/globalscope.xsl"));
+		this.validator.validate(new DOMSource(document));
+	}
+	
+	public final void testHandlers() throws Exception {
+		Document document = builder.parse(new File("xslt/handlers.xsl"));
+		this.validator.validate(new DOMSource(document));
+	}
+	
+	public final void testInvoke() throws Exception {
+		Document document = builder.parse(new File("xslt/invoke.xsl"));
+		this.validator.validate(new DOMSource(document));
+	}
+
+	public final void testIRRA() throws Exception {
+		Document document = builder.parse(new File("xslt/irra.xsl"));
+		this.validator.validate(new DOMSource(document));
+	}
+
+	public final void testJoinTransition() throws Exception {
+		Document document = builder.parse(new File("xslt/jointransition.xsl"));
+		this.validator.validate(new DOMSource(document));
+	}
+
+	public final void testLanguage() throws Exception {
+		Document document = builder.parse(new File("xslt/language.xsl"));
+		this.validator.validate(new DOMSource(document));
+	}
+
+	public final void testReceive() throws Exception {
+		Document document = builder.parse(new File("xslt/receive.xsl"));
+		this.validator.validate(new DOMSource(document));
+	}
+
+	public final void testRepeatUntil() throws Exception {
+		Document document = builder.parse(new File("xslt/repeatuntil.xsl"));
+		this.validator.validate(new DOMSource(document));
+	}
+
+	public final void testReply() throws Exception {
+		Document document = builder.parse(new File("xslt/reply.xsl"));
+		this.validator.validate(new DOMSource(document));
+	}
+	
+	public final void testToFromParts() throws Exception {
+		Document document = builder.parse(new File("xslt/tofromparts.xsl"));
+		this.validator.validate(new DOMSource(document));
+	}
+
+	public final void testVariables() throws Exception {
+		Document document = builder.parse(new File("xslt/variables.xsl"));
+		this.validator.validate(new DOMSource(document));
+	}
+
+
+}
