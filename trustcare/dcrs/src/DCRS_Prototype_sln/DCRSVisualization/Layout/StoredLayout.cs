6d94e6360d6f0d57b4a6d843f2f974b24af63901
tslaats <tslaats@d463407e-c81c-0410-9dde-f82ba9d86a8d>
2010-10-13 14:40:15 +0000

    started work on a stored layout, not really finished yet.
    git-svn-id: svn+ssh://ssh.itu.dk/import/stud/share/projects/bpl/svn/trunk/impl@3731 d463407e-c81c-0410-9dde-f82ba9d86a8d

---
+++ trustcare/dcrs/src/DCRS_Prototype_sln/DCRSVisualization/Layout/StoredLayout.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ITU.DK.DCRS.Visualization.Layout
+{
+    public class StoredLayout<ST, LT> : LayoutProvider<ST, LT> where ST : IEquatable<ST>
+    {
+        Placement<ST> placement;
+        
+        public void Run()
+        {
+            placement = Placement<ST>.DeserializeFromXMLOld();
+        }
+
+        public Dictionary<ST, Vector2> GetNodePositions()
+        {
+            Dictionary<ST, Vector2> result = new Dictionary<ST, Vector2>();
+            /*
+            foreach (var x in placement.Locations)
+                result.Add(x.Key, new Vector2(x.Value));
+            return result;
+            */
+
+            for (int i = 0; i < placement.Keys.Count; i++)
+            {
+                result.Add(placement.Keys.ElementAt(i), new Vector2(placement.Values.ElementAt(i)));
+            }
+            return result;
+        }
+    }
+}
---
+++ trustcare/dcrs/src/DCRS_Prototype_sln/DCRSVisualization/Placement.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace ITU.DK.DCRS.Visualization
+{
+    public class Placement<ST> where ST : IEquatable<ST>
+    {
+        public int processID;
+        public int instanceID;
+        //public Dictionary<ST, Point> Locations;
+        //public List<KeyValuePair<ST, Point>> Locations;
+
+        public List<ST> Keys;
+        public List<Point>Values;
+
+        public Placement()
+        {
+            //Locations = new Dictionary<ST, Point>();
+            //Locations = new List<KeyValuePair<ST, Point>>();
+            Keys = new List<ST>();
+            Values = new List<Point>();
+        }
+
+
+        public void Add(ST n, Point p)
+        {
+            Keys.Add(n);
+            Values.Add(p);
+        }
+
+
+        static public void SerializeToXML(Placement<ST> p)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Placement<ST>));
+            TextWriter textWriter = new StreamWriter(@"C:\PhDWork\placements\placement#" + p.processID.ToString() + "#" + p.instanceID.ToString() + "#.xml");
+            serializer.Serialize(textWriter, p);
+            textWriter.Close();
+        }
+
+        static public Placement<ST> DeserializeFromXML(int processId, int instancId)
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(Placement<ST>));
+            TextReader textReader = new StreamReader(@"C:\PhDWork\placements\placement#" + processId.ToString() + "#" + instancId.ToString() + "#.xml");
+            Placement<ST> p;
+            p = (Placement<ST>)deserializer.Deserialize(textReader);
+            textReader.Close();
+
+            return p;
+        }
+
+
+
+        static public void SerializeToXMLOld(Placement<ST> p)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Placement<ST>));
+            TextWriter textWriter = new StreamWriter(@"C:\PhDWork\placement.xml");
+            serializer.Serialize(textWriter, p);
+            textWriter.Close();
+        }
+
+
+        static public Placement<ST> DeserializeFromXMLOld()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(Placement<ST>));
+            TextReader textReader = new StreamReader(@"C:\PhDWork\placement.xml");
+            Placement<ST> p;
+            p = (Placement<ST>)deserializer.Deserialize(textReader);
+            textReader.Close();
+
+            return p;
+        }
+    }
+}
