c184e9c79e4df0ce387e8224e177c871f8a0ee00
jadu <jadu@d463407e-c81c-0410-9dde-f82ba9d86a8d>
2010-10-20 13:23:23 +0000

    git-svn-id: svn+ssh://ssh.itu.dk/import/stud/share/projects/bpl/svn/trunk/impl@3767 d463407e-c81c-0410-9dde-f82ba9d86a8d

---
+++ trustcare/dcrs/src/DCRS_Prototype_sln/WorkflowEngineHost/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using ITU.DK.DCRS.CommonTypes.ServiceContracts;
+using ITU.DK.DCRS.WorkflowEngine.Core;
+
+namespace SampleConsoleTestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var workflowEngineInstance = DCRSWorkflowEngine.WorkflowEngineInstance;
+
+
+                Console.WriteLine("Hosted the DCRS workflow engine successfully!");
+
+                while (true)
+                {
+                    var line = Console.ReadLine();
+
+                    if (line != null) if(line.Equals("exit")) break;
+                }
+
+            }
+            catch (Exception exception)
+            {
+
+                Console.WriteLine("Failed to host workflow engine. Error message:{0}", exception.Message);
+            }
+        }
+    }
+}
