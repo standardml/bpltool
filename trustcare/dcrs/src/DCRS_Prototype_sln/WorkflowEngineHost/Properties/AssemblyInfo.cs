e9c061688ea1f018bda3e7d65a873faa3b35f2eb
jadu <jadu@d463407e-c81c-0410-9dde-f82ba9d86a8d>
2010-10-20 13:53:12 +0000

    Change WorkflowEngineHost assembly name.
    git-svn-id: svn+ssh://ssh.itu.dk/import/stud/share/projects/bpl/svn/trunk/impl@3768 d463407e-c81c-0410-9dde-f82ba9d86a8d

--- trustcare/dcrs/src/DCRS_Prototype_sln/WorkflowEngineHost/Properties/AssemblyInfo.cs
+++ trustcare/dcrs/src/DCRS_Prototype_sln/WorkflowEngineHost/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SampleConsoleTestApplication")]
+[assembly: AssemblyTitle("WorkflowEngineHost")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SampleConsoleTestApplication")]
+[assembly: AssemblyProduct("WorkflowEngineHost")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
